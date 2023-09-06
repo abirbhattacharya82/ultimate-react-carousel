bd642c168db440461ac5c545a6d320625a5b8f74
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-09-06 03:15:19 +0000

    Starter

---
+++ super-react-carasoul/src/Components/Carousel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Carousel.css';
+function Carousel() {
+    return (
+        <div className="Carousel">
+            <div className="Slider-Wrapper">
+                <div className="Slider">
+                    
+                </div>
+            </div>
+        </div>
+    )
+};
+export default Carousel;
